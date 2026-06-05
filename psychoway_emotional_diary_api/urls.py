a477a7039b55a1012da9ec63834d955c769a51bf
lui0slpk <itslucky535@gmail.com>
2026-06-04 19:45:12 -0500

    Se agregaron las urls de los modelos

--- psychoway_emotional_diary_api/urls.py
+++ psychoway_emotional_diary_api/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('user_auth.urls')),          # Rutas de usuarios
+    path('api/', include('emotional_diary.urls')),    # Rutas del diario, entradas y emociones
 ]
